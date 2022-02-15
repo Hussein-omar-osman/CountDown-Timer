9c3c7b4d0808ae9a57a39f5d3137dbfab20e152c
Hussein Omar <husseinomar6190@gmail.com>
2022-02-15 20:59:25 +0300

    adding functionality

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log('hello');
+const newYear = '1 Jan 2023';
+
+function countDown() {
+  const newYearsDate = new Date(newYear);
+  const currentDate = new Date();
+
+  const totalSeconds = Math.floor((newYearsDate - currentDate) / 1000);
+
+  const days = Math.floor(totalSeconds / 3600 / 24);
+  const hours = Math.floor(totalSeconds / 3600) % 24;
+  const minutes = Math.floor(totalSeconds / 60) % 60;
+  const seconds = Math.floor(totalSeconds) % 60;
+  console.log(totalSeconds, days, hours, minutes, seconds);
+}
+
+countDown();
+
+// setInterval(countDown, 1000);
