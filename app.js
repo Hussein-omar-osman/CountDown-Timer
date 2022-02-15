585b7e3739a47eeef3d840db5a44f9cfb0d70e34
Hussein Omar <husseinomar6190@gmail.com>
2022-02-15 22:43:45 +0300

    adding birthday calc

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const newYear = '1 Jan 2023';
+let newYear = '1 January 2023';
 const daysEl = document.getElementById('days');
 const hoursEl = document.getElementById('hours');
 const minutesEl = document.getElementById('minutes');
@@ -17,11 +17,29 @@ function countDown() {
   // console.log(totalSeconds, days, hours, minutes, seconds);
 
   daysEl.innerHTML = days;
-  hoursEl.innerHTML = hours;
-  minutesEl.innerHTML = minutes;
-  secondsEl.innerHTML = seconds;
+  hoursEl.innerHTML = formatTime(hours);
+  minutesEl.innerHTML = formatTime(minutes);
+  secondsEl.innerHTML = formatTime(seconds);
 }
 
 countDown();
 
+function formatTime(time) {
+  if (time < 10) {
+    return (time = `0${time}`);
+  } else {
+    return (time = time);
+  }
+}
+
 setInterval(countDown, 1000);
+
+const dateBorn = document.getElementById('dateBorn');
+const monthBorn = document.getElementById('monthBorn');
+const yearBorn = document.getElementById('yearBorn');
+const h1 = document.getElementById('h1');
+const btn = document.getElementById('submit');
+btn.addEventListener('click', function () {
+  newYear = `${dateBorn.value} ${monthBorn.value} 2022`;
+  h1.innerText = 'Days Remaining';
+});
