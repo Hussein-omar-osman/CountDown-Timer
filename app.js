9ce5dd5fd9ed3f2d84a50161b5a9dcbeb3fb8c7d
Hussein Omar <husseinomar6190@gmail.com>
2022-02-15 21:10:45 +0300

    conecting to the DOM

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 const newYear = '1 Jan 2023';
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
 
 function countDown() {
   const newYearsDate = new Date(newYear);
@@ -10,9 +14,14 @@ function countDown() {
   const hours = Math.floor(totalSeconds / 3600) % 24;
   const minutes = Math.floor(totalSeconds / 60) % 60;
   const seconds = Math.floor(totalSeconds) % 60;
-  console.log(totalSeconds, days, hours, minutes, seconds);
+  // console.log(totalSeconds, days, hours, minutes, seconds);
+
+  daysEl.innerHTML = days;
+  hoursEl.innerHTML = hours;
+  minutesEl.innerHTML = minutes;
+  secondsEl.innerHTML = seconds;
 }
 
 countDown();
 
-// setInterval(countDown, 1000);
+setInterval(countDown, 1000);
